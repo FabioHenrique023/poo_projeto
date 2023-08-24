ade4646b5e74be0c1dab80d43b718cce12a32103
FabioHenrique023 <fabiohomeoffice904@gmail.com>
2023-08-24 20:11:48 -0300

    corrigindo bug main.py

--- poo_projeto/main.py
+++ poo_projeto/main.py
@@ -1,6 +1,7 @@
-from poo_projeto.user import Usuario
+from user import Usuario
 
 user = Usuario("Fabio", "Fabio", "123456", "ADM", "fabio@", "0909")
+#user = Usuario()
 
 user.cadastrar_usario("Cadastrar")
 ##user.listar_usuario("Listar")
--- poo_projeto/user.py
+++ poo_projeto/user.py
@@ -1,5 +1,5 @@
 class Usuario:
-    def _init_(self, nome, login, senha, tipo, email, telefone):
+    def __init__(self, nome, login, senha, tipo, email, telefone):
         self.nome = nome
         self.login = login
         self.senha = senha
@@ -8,16 +8,16 @@ class Usuario:
         self.telefone = telefone
 
     def cadastrar_usario(self, info):
-        user = "Fabio"
+        user = "Fabio H"
         if user == self.nome:
              return print(f'Usuário {self.nome} já cadastrado')
         return print(f'Usuário {self.nome} cadastrado com sucesso')
     
-    # def listar_usuario(self, info):
-    #     existe = "Fabio"
-    #     if existe == self.nome:
-    #         return print(f'Usuário existe: {self.nome}, {self.email}')
-    #     return print("Usuário não existe")
+    def listar_usuario(self, info):
+        existe = "Fabio"
+        if existe == self.nome:
+            return print(f'Usuário existe: {self.nome}, {self.email}')
+        return print("Usuário não existe")
     
     # def alterar_usuario(self, info):
     #     altera = "Tiago"
