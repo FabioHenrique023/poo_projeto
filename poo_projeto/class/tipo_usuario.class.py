f098b698bf7300deb869c96dec2ed344cfff091e
FabioHenrique023 <fabiohomeoffice904@gmail.com>
2023-09-18 13:47:34 -0300

    resolvendo commit

--- poo_projeto/class/tipo_usuario.class.py
+++ poo_projeto/class/tipo_usuario.class.py
@@ -1,10 +1,36 @@
+from conexao import Conexao as bd
+import psycopg2
+
+
 class TipoUsuario:
-    def __init__(self, id, nome):
-        self.id = id
+    def __init__(self, nome):
         self.nome = nome
         
+    def usuer_existe(self):
+        try:
+            conexao = bd().conectar()
+            cursor = conexao.cursor()
+            
+            #CONSULTAR USUÁRIO
+            sql = "SELECT id FROM tipo_usuario WHERE nome = %s;"
+            
+            cursor.execute(sql, (self.nome,))
+            
+            resultado = cursor.fetchone()
+            
+            #FECHAR A CONEXÃO
+            cursor.close()
+            conexao.close()
+            
+            return resultado is not None
+        
+        except psycopg2.Error as error:
+            print(f"Não foi possível criar o usuário {error}")
+                
     def create_tipousuario(self):
-        pass 
+        if not self.usuer_existe:
+            try:
+                
     
     def update_tipousuario(self):
         pass
