d4f67402db85e9cbb36bc8a9a443ae7b7c6d0a67
FabioHenrique023 <fabiohomeoffice904@gmail.com>
2023-09-18 13:40:19 -0300

    alterando classe de conexao

--- poo_projeto/conexao.py
+++ poo_projeto/conexao.py
@@ -1,33 +1,47 @@
 import psycopg2
 
-def conectar():
-    try:
-        conexao = psycopg2.connect(
-            host = "localhost",
-            database = "biblioteca",
-            user = "postgres",
-            password = "123456"
-        )
-        cursor = conexao.cursor()
-        return conexao, cursor
-    except Exception as erro:
-        print(f"Erro ao conectar ao banco {erro}")
-        return None, None
+class Conexao:
+    def __init__(self, host, database, user, password):
+        self.host = host
+        self.database = database
+        self.user = user
+        self.password = password
+        self.conexao = None
+        self.cursor = None
 
-def desconectar(cursor, conexao):
-    if conexao:
-        conexao.close()
-    if cursor:
-        cursor.close()
-        
-#TESTANDO A CONEXÃO
+    def conectar(self):
+        try:
+            self.conexao = psycopg2.connect(
+                host=self.host,
+                database=self.database,
+                user=self.user,
+                password=self.password
+            )
+            self.cursor = self.conexao.cursor()
+        except Exception as erro:
+            print(f"Erro ao conectar ao banco: {erro}")
 
-# conexao, cursor = conectar()
+    def desconectar(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.conexao:
+            self.conexao.close()
 
-# if conexao:
-#     cursor.execute("SELECT * FROM tipo_usuario")
-#     registros = cursor.fetchall()
-#     for i in registros:
-#         print(i)
+# Exemplo de uso
+conexao_config = {
+    "host": "localhost",
+    "database": "biblioteca",
+    "user": "postgres",
+    "password": "123456"
+}
 
-#     desconectar(conexao, cursor)        
+conexao = Conexao(**conexao_config)
+conexao.conectar()
+
+if conexao.conexao:
+    conexao.cursor.execute("SELECT * FROM tipo_usuario")
+    registros = conexao.cursor.fetchall()
+    for i in registros:
+        print(i)
+
+conexao.desconectar()
